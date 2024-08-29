1675acdcb5b0f2d5dfbf5042ca54e395a4829d86
Oytun <140172465+oytunongan@users.noreply.github.com>
2024-08-29 17:37:14 +0300

    Add files via upload

---
+++ favorites/favorites.sql
@@ -0,0 +1,5 @@
+UPDATE shows SET title = "Adventure Time" WHERE title LIKE "adven%";
+UPDATE shows SET title = "Avatar:The Last Airbender" WHERE title LIKE "ava%";
+UPDATE shows SET title = "Arrow" WHERE title LIKE "arro%";
+UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title LIKE "broo%";
+UPDATE shows SET title = "Community" WHERE title LIKE "com%";
